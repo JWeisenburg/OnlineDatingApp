39f40775679c1e5fb0b4bbfdd61fc796051bbd81
Your Name <you@example.com>
2020-05-29 09:42:08 -0400

    push changes to heroku

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MongoDB: 'mongodb+srv://joshua:xcom1122@cluster0-5xfy3.mongodb.net/test?retryWrites=true&w=majority',
+  FacebookAppID: '1126584071043668',
+  FacebookAppSecret: 'dbe86fefd3e4fa7f5acde1b45d457a59',
+  GoogleClientID: '516312251465-ovumom9rfcg27j7bem84egsmpufkkpte.apps.googleusercontent.com',
+  GoogleClientSecret: 'AptEM6x-kZEwd14AgIvNX6gm'
+
+};
