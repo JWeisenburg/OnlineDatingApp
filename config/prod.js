01771c68c27aa6c685fc9e56224f00ede1a3cf37
Your Name <you@example.com>
2020-05-29 12:24:51 -0400

    facebook app update

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   MongoDB: process.env.MONGO_DB,
-  FacebookAppID: '1126584071043668',
-  FacebookAppSecret: 'dbe86fefd3e4fa7f5acde1b45d457a59',
+  FacebookAppID: process.env.FB_APP_ID,
+  FacebookAppSecret: process.env.FB_APP_SECRET,
   GoogleClientID: '516312251465-ovumom9rfcg27j7bem84egsmpufkkpte.apps.googleusercontent.com',
   GoogleClientSecret: 'AptEM6x-kZEwd14AgIvNX6gm'
 
