6c5b327508ebc7de3f725cdc748d09d5d5511987
Your Name <you@example.com>
2020-05-29 11:06:45 -0400

    mongoDB link updated

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MongoDB: 'mongodb+srv://joshua:xcom1122@cluster0-5xfy3.mongodb.net/test?retryWrites=true&w=majority',
+  MongoDB: process.env.MONGO_DB,
   FacebookAppID: '1126584071043668',
   FacebookAppSecret: 'dbe86fefd3e4fa7f5acde1b45d457a59',
   GoogleClientID: '516312251465-ovumom9rfcg27j7bem84egsmpufkkpte.apps.googleusercontent.com',
