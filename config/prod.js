dd90a9b72d7b3e8d347a5e622642db9031e55764
Your Name <you@example.com>
2020-05-30 02:00:34 -0400

    Google client id

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
   MongoDB: process.env.MONGO_DB,
   FacebookAppID: process.env.FB_APP_ID,
   FacebookAppSecret: process.env.FB_APP_SECRET,
-  GoogleClientID: '516312251465-ovumom9rfcg27j7bem84egsmpufkkpte.apps.googleusercontent.com',
-  GoogleClientSecret: 'AptEM6x-kZEwd14AgIvNX6gm'
+  GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+  GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 
 };
