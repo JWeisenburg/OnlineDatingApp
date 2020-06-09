4ecb4488506cb4d2a2046e443eb03a7ce837727d
Your Name <you@example.com>
2020-06-09 01:19:12 -0400

    ?

--- server.js
+++ server.js
@@ -189,6 +189,20 @@ app.post('/uploadPictures',requireLogin,(req,res) =>{
         })
       })
 })
+//delete picture
+app.get('/deletePicture/:id',requireLogin,(req,res) =>{
+  User.findById({_id:req.user._id})
+  .then((user) =>{
+    user.pictures.id(req.params.id).remove()
+    user.save((err) =>{
+      if (err) {
+        throw err;
+      }else{
+        res.redirect('/profile');
+      }
+    })
+  })
+})
 app.post('/updateProfile', requireLogin, (req, res) => {
   User.findById({
       _id: req.user._id
