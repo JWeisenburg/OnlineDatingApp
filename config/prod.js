72d0c062951f8bcbee05fdb172e6031b88f2bf72
Your Name <you@example.com>
2020-06-10 00:23:39 -0400

    a

--- config/prod.js
+++ config/prod.js
@@ -4,8 +4,8 @@ module.exports = {
   FacebookAppSecret: process.env.FB_APP_SECRET,
   GoogleClientID: process.env.GOOGLE_CLIENT_ID,
   GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  AWSAccessKeyID: 'AKIAXAOFQXVX5E25IPCY ',
-  AWSAccessKeySecret:'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',
+  AWSAccessKeyID: process.env.AWS_ACCESS_KEY,
+  AWSAccessKeySecret: process.env.AWS_ACCESS_SECRET,
   StripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   StripeSecretKey: process.env.STRIPE_SECRET_KEY
   };
